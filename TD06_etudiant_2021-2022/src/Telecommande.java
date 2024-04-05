bc1246bff705f41da3a735abfa84a981d850fc06
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-04-05 15:11:13 +0200

    Ajout le squelette de la classe Telecommande

--- TD06_etudiant_2021-2022/src/Telecommande.java
+++ TD06_etudiant_2021-2022/src/Telecommande.java
@@ -1,2 +1,31 @@
+import java.util.*;
+
 public class Telecommande {
+
+    private List<Lampe> lampes;
+
+    public Telecommande() {
+        lampes = new ArrayList<>();
+    }
+
+    public void ajouterLampe(Lampe lampe) {
+        throw new Error("code non ecrit");
+    }
+
+    public void activerLampe(int indiceLampe) {
+        throw new Error("code non ecrit");
+    }
+
+    public void desactiverLampe(int indiceLampe) {
+        throw new Error("code non ecrit");
+    }
+
+    public void activerTout() {
+        throw new Error("code non ecrit");
+    }
+
+    @Override
+    public String toString() {
+        throw new Error("code non ecrit");
+    }
 }
