7ca5d1caae3992d6a193cc9bc8dceaeb5399d20d
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-04-05 15:31:14 +0200

    Ecrire le contenu des methodes de la classe Telecommande

--- TD06_etudiant_2021-2022/src/Telecommande.java
+++ TD06_etudiant_2021-2022/src/Telecommande.java
@@ -4,29 +4,45 @@ public class Telecommande {
 
     private List<Lampe> lampes;
 
+
     public Telecommande() {
         lampes = new ArrayList<>();
     }
 
     public void ajouterLampe(Lampe lampe) {
-        throw new Error("code non ecrit");
+        lampes.add(lampe);
     }
 
     public void activerLampe(int indiceLampe) {
-        throw new Error("code non ecrit");
+        if (indiceLampe < 0 || indiceLampe >= lampes.size()) {
+            throw new IllegalArgumentException("Lampe inexistante");
+        }
+        lampes.get(indiceLampe).allumer();
     }
 
     public void desactiverLampe(int indiceLampe) {
-        throw new Error("code non ecrit");
+        if (indiceLampe < 0 || indiceLampe >= lampes.size()) {
+            throw new IllegalArgumentException("Lampe inexistante");
+        }
+        lampes.get(indiceLampe).eteindre();
     }
 
     public void activerTout() {
-        throw new Error("code non ecrit");
+        for (Lampe lampe : lampes) {
+            lampe.allumer();
+        }
     }
 
     @Override
     public String toString() {
-        throw new Error("code non ecrit");
+        StringBuilder sb = new StringBuilder();
+        for (Lampe lampe : lampes) {
+            sb.append(lampe.toString()).append("\n");
+        }
+        return sb.toString();
     }
 
+    public List<Lampe> getLampes() {
+        return lampes;
+    }
 }
