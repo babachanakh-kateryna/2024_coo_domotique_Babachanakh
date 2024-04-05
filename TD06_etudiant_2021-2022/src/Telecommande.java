8909e2c4412851a85f25fa5feaf6240306bb8972
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-04-05 15:46:16 +0200

    Modifier la classe Telecommande pour manipuler des chaines Hifi

--- TD06_etudiant_2021-2022/src/Telecommande.java
+++ TD06_etudiant_2021-2022/src/Telecommande.java
@@ -3,14 +3,20 @@ import java.util.*;
 public class Telecommande {
 
     private List<Lampe> lampes;
+    private List<Hifi> hifis;
 
 
     public Telecommande() {
         lampes = new ArrayList<>();
+        hifis = new ArrayList<>();
     }
 
     public void ajouterLampe(Lampe lampe) {
         lampes.add(lampe);
+    }
+
+    public void ajouterHifi(Hifi hifi) {
+        hifis.add(hifi);
     }
 
     public void activerLampe(int indiceLampe) {
@@ -20,6 +26,14 @@ public class Telecommande {
         lampes.get(indiceLampe).allumer();
     }
 
+    public void activerHifi(int indiceHifi) {
+        if (indiceHifi >= 0 && indiceHifi < hifis.size()) {
+            hifis.get(indiceHifi).allumer();
+        } else {
+            System.out.println("Hifi inexistante");
+        }
+    }
+
     public void desactiverLampe(int indiceLampe) {
         if (indiceLampe < 0 || indiceLampe >= lampes.size()) {
             throw new IllegalArgumentException("Lampe inexistante");
@@ -27,17 +41,35 @@ public class Telecommande {
         lampes.get(indiceLampe).eteindre();
     }
 
+    public void desactiverHifi(int indiceHifi) {
+        if (indiceHifi >= 0 && indiceHifi < hifis.size()) {
+            hifis.get(indiceHifi).eteindre(); // Метод для выключения аудиосистемы
+        } else {
+            System.out.println("Hifi inexistante");
+        }
+    }
+
+
     public void activerTout() {
         for (Lampe lampe : lampes) {
             lampe.allumer();
+        }
+        for (Hifi hifi : hifis) {
+            hifi.allumer();
         }
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        sb.append("Telecommande:\n");
+        sb.append("Lampes:\n");
         for (Lampe lampe : lampes) {
-            sb.append(lampe.toString()).append("\n");
+            sb.append(" - ").append(lampe.toString()).append("\n");
+        }
+        sb.append("Hifis:\n");
+        for (Hifi hifi : hifis) {
+            sb.append(" - ").append(hifi.toString()).append("\n");
         }
         return sb.toString();
     }
